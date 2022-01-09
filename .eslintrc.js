b77f09defd76cdd756f3391d883aa89993aaf42c
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-09 19:41:22 -0300

    chore: initial project structure

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'eslint-plugin-import-helpers'],
+  ignorePatterns: ['node_modules', '/*.js', 'dist'],
+  rules: {
+    camelcase: 'off',
+    'prettier/prettier': 'error',
+    'import/no-unresolved': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
+      },
+    ],
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'no-shadow': 'off',
+    'no-console': 'off',
+    'no-useless-constructor': 'off',
+    'no-empty-function': 'off',
+    'lines-between-class-members': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+      },
+    ],
+    'import-helpers/order-imports': ['warn'],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: { },
+    },
+  },
+};
