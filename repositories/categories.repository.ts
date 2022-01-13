e93d31dc8320c6b2f206cb8189ac5b277c1fea14
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-13 18:09:28 -0300

    feat: create a categories repository and add dto concept to improve responsability segregation

---
+++ repositories/categories.repository.ts
@@ -0,0 +1,26 @@
+import { Category } from '../model/Category';
+
+//DTO => Data Transfer Object, responsible to transited with data across the layers of the app
+// The routes does not nedd to be aware of the models classes types
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[] = [];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  createCategory({ description, name }: ICreateCategoryDTO): void {
+    const category = new Category();
+    Object.assign(category, { name, description, created_at: new Date() });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepository };
