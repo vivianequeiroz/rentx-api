a005c1d24ab634b0d58c88052a516caf41c1590b
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-16 15:10:38 -0300

    feat: add implementations folder to improve organization

--- routes/categories.routes.ts
+++ routes/categories.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handleRequest(request, response);
