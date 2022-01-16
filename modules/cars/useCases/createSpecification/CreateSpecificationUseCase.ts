c9a41a6825e9a0d75427cca4003a2612416ffb74
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-16 15:25:20 -0300

    feat: add specifications use case

--- modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,10 +1,10 @@
-import { ISpecificationRepository } from '../repositories/ISpecificationRepository';
+import { ISpecificationRepository } from '../../repositories/ISpecificationRepository';
 
 interface IRequest {
   name: string;
   description: string;
 }
-class CreateSpecificationService {
+class CreateSpecificationUseCase {
   constructor(private specificationRepository: ISpecificationRepository) {}
 
   execute({ name, description }: IRequest): void {
@@ -18,4 +18,4 @@ class CreateSpecificationService {
     this.specificationRepository.createSpecification({ name, description });
   }
 }
-export { CreateSpecificationService };
+export { CreateSpecificationUseCase };
