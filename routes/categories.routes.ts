d06e688ac63a34b5657bf3958abd9e9d1fb87a5b
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-17 18:34:28 -0300

    feat: add route to upload a file

--- routes/categories.routes.ts
+++ routes/categories.routes.ts
@@ -1,11 +1,13 @@
-import { Router } from 'express';
+import { response, Router } from 'express';
+import multer from 'multer';
 
-import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = CategoriesRepository.getInstance();
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handleRequest(request, response);
@@ -15,4 +17,11 @@ categoriesRoutes.get('/', (request, response) => {
   return listCategoriesController.handleRequest(request, response);
 });
 
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  const { file } = request;
+
+  console.log(file);
+
+  return response.send();
+});
 export { categoriesRoutes };
