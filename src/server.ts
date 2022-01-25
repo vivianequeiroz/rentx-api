23fc00851cca83166a047551593941ce39996fa9
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-25 16:29:06 -0300

    feat: add initial connection and config file to database

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 
-import { router } from '../routes';
+import { router } from './routes';
 import swaggerFile from './swagger.json';
 import swaggerUi from 'swagger-ui-express';
+import './database';
 
 const app = express();
 
