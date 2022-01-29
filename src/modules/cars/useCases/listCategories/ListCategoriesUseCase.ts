d96d418940d80b6d2ef4a15f6cda82932036713d
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-29 19:57:16 -0300

    fix: add promise into function type

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,13 +1,18 @@
+import { inject, injectable } from 'tsyringe';
 import { Category } from '../../entities/Category';
 import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
 
+@injectable()
 class ListCategoriesUseCase {
-  constructor(private categoriesRepository: CategoriesRepository) {}
+  constructor(
+    @inject('CategoriesRepository')
+    private categoriesRepository: CategoriesRepository,
+  ) {}
 
-  execute(): Category[] {
-    const categoriesFound = this.categoriesRepository.listCategories();
+  execute(): Promise<Category[]> {
+    const categories = this.categoriesRepository.listCategories();
 
-    return categoriesFound;
+    return categories;
   }
 }
 
