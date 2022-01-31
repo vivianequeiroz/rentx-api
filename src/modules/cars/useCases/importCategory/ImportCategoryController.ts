3a22e836b6ea6626e7ba90690330ca3a333df9ce
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-31 17:57:05 -0300

    refact: add dependency injection using tysringe at import category

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,13 +1,13 @@
+import { container } from 'tsyringe';
 import { Request, Response } from 'express';
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
 class ImportCategoryController {
-  constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
-
   handleRequest(request: Request, response: Response): Response {
     const { file } = request;
+    const importCategoryUseCase = container.resolve(ImportCategoryUseCase);
 
-    this.importCategoryUseCase.execute(file!);
+    importCategoryUseCase.execute(file!);
 
     return response.send();
   }
