463bab6a9c75bfa8dc1db9e36892e053442ac457
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-02-01 19:06:13 -0300

    refact: add typeorm decorator to map specification properties

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,9 +1,17 @@
+import { Column, PrimaryColumn, CreateDateColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
 class Specification {
-  name!: string;
-  description!: string;
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  description!: string;
+
+  @CreateDateColumn()
   created_at!: Date;
 
   constructor() {
